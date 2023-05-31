104deb565ed7efbcd6c28ce7b6a80d33e51f2685
gaditavitinaidu928 <gaditavitinaidu928@gmail.com>
2023-05-31 14:56:14 +0530

    Addition

---
+++ src/com/satya/addition/AdditionEx.java
@@ -0,0 +1,12 @@
+package com.satya.addition;
+
+public class AdditionEx {
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int result;
+		result=num1+num2;
+		System.out.println("Addition of two numbers..."+result);
+	}
+
+}
